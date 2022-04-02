66bbef38b235c307a53aa7e4e259a81518ad5aa8
B-lovedth <greatsoma2019@gmail.com>
2022-04-02 13:58:40 +0100

    Added section to the report component

--- src/components/report/Report.js
+++ src/components/report/Report.js
@@ -4,7 +4,17 @@ import React from 'react'
 const Report = () => {
   return (
     <div>
-      REPORT
+      <h2>This is our monthly report</h2>
+      <div>
+        <section className='section-body'>
+          <div>Section One</div>
+          <div>Section Two</div>
+          <div>Section Three</div>
+          <div>Section Four</div>
+          <div>Section Five</div>
+        </section>
+      </div>
+
     </div>
   )
 }
