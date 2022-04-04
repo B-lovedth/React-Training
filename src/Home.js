9a48bd852192eb828651b91f381f3725b7275156
B-lovedth <greatsoma2019@gmail.com>
2022-04-04 14:48:47 +0100

    sub commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Home = () => {
+    const myClick = (name)=>{
+            alert(`Hello ${name}`)
+    }
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      <button onClick={()=>{
+          myClick("Great")
+      }}>Click me!</button>
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     <nav className="navbar">
         <h1>The Soma Blog</h1>
         <div className="links">
-            <a href="home.com">Home</a>
-            <a href="blog.com">New Blog</a>
+            <a href="/home">Home</a>
+            <a href="/blog" className=''>New Blog</a>
         </div>
     </nav>
   )
