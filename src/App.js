84a01fb556efa8d048b640847cefa4c871d7fad4
B-lovedth <greatsoma2019@gmail.com>
2022-04-04 12:52:18 +0100

    subsequent commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css'
 
 function App() {
   const myString = `App Component`
+  const likes = 90
   return (
    <div className="App">
      <div className="content">
        <h1>{ myString }</h1>
+       <p>LIked {likes} times</p>
      </div>
     </div> 
   );
