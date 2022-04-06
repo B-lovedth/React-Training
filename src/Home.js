df0d88f4db0ca0b47604c94c9d3caa246efd9dd4
B-lovedth <greatsoma2019@gmail.com>
2022-04-06 20:43:06 +0100

    sub commit

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
-    const myClick = (name)=>{
-            alert(`Hello ${name}`)
-    }
+    const [blogs,setBlogs] = useState([
+        {title:'My new website',body:'lorem ipsum... ', author:'Great',id:'1'},
+        {title:'Welcome party',body:'lorem ipsum... ', author:'Belovedth',id:'2'},
+        {title:'Web dev tool tips',body:'lorem ipsum... ', author:'Soma',id:'3'}
+    ])
   return (
     <div className="Home">
-      <h2>Homepage</h2>
-      <button onClick={()=>{
-          myClick("Great")
-      }}>Click me!</button>
+        {blogs.map((myblog)=>(
+            <div className="blog-preveiw" key={myblog.id}>
+                <h2>{myblog.title}</h2>
+                <p>Written by { myblog.author }</p>
+            </div>
+        ))}
     </div>
   )
 }
