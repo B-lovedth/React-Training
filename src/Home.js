6028a7786b1a07a06f1122348041dd953dcfbe39
B-lovedth <greatsoma2019@gmail.com>
2022-04-09 08:35:47 +0100

    sub commit

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,13 @@ import React, { useState,useEffect } from 'react'
 import Bloglist from './Bloglist'
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
-        {title:'My new website',body:'lorem ipsum... ', author:'Great',id:'1'},
-        {title:'Welcome party',body:'lorem ipsum... ', author:'Belovedth',id:'2'},
-        {title:'Web dev tool tips',body:'lorem ipsum... ', author:'Soma',id:'3'}
-    ])
+    const [blogs,setBlogs] = useState(null)
     const handleDelete = (id) => {
       const newBlogs = blogs.filter((blog)=> blog.id !== id)
       setBlogs(newBlogs)
     }
     useEffect(() => {
-
-      console.log(' use effect ran',);
+      fetch()
     })
   return (
     <div className="Home">
