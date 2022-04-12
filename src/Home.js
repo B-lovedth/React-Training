3beecd1fae14f7b06fea904c4bef6710ba12b376
B-lovedth <greatsoma2019@gmail.com>
2022-04-12 20:07:35 +0100

    sub commit

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,8 @@ import React, { useState,useEffect } from 'react'
 import Bloglist from './Bloglist'
 
 const Home = () => {
-    const url = 'http://localhost:8000/blogs'
-    const [blogs,setBlogs] = useState()
-    const [isPending,setIsPending] = useState(true)
-    const [error,setError] = useState(null)
-    useEffect(() => {
-      fetch(url)
-      .then(res => { 
-        if(!res.ok){ 
-          throw Error('Could not fetch the data for that resource')
-      }
-      return res.json()}
-      )
-      .then(data => {
-        setBlogs(data)
-        setIsPending(false)
-        setError(null)
-      }
-      ).catch(err=>{
-        setError(err.message)
-        setIsPending(false)
-        setBlogs()
-      })
-    },[]
-    )
+   
+   
   return (
     <div className="Home">
       {error && <div>{ error }</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+
+const useFetch = () =>{
+    const [blogs,setBlogs] = useState()
+    const [isPending,setIsPending] = useState(true)
+    const [error,setError] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => { 
+          if(!res.ok){ 
+            throw Error('Could not fetch the data for that resource')
+        }
+        return res.json()}
+        )
+        .then(data => {
+          setBlogs(data)
+          setIsPending(false)
+          setError(null)
+        }
+        ).catch(err=>{
+          setError(err.message)
+          setIsPending(false)
+          setBlogs()
+        })
+      },[]
+      )
+}
