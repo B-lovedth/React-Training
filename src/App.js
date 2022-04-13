73bb971d9fcdb22b7435f85ef22f8ffa1303dd79
B-lovedth <greatsoma2019@gmail.com>
2022-04-13 19:42:39 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <Navbar/>
       <div className="content">
         <Switch>
-          <Route path='/home'>
+          <Route path='/'>
               <Home/>
           </Route>
         </Switch>
