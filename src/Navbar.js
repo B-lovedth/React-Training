164396934e8d21afe5e5a0bc35410467e8446e33
B-lovedth <greatsoma2019@gmail.com>
2022-04-13 20:11:35 +0100

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <h1>The Soma Blog</h1>
         <div className="links">
             <a href="/">Home</a>
-            <a href="/blog" className=''>New Blog</a>
+            <a href="/create" className=''>New Blog</a>
         </div>
     </nav>
   )
