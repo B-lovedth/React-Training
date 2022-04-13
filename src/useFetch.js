ee93c26f3f0134c3ca68c8ecb7e60dc9aeedb62a
B-lovedth <greatsoma2019@gmail.com>
2022-04-13 20:55:23 +0100

    changes

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) =>{
     const [error,setError] = useState(null)
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController()
+
+        fetch(url, {signal: abortCont.signal})
         .then(res => { 
           if(!res.ok){ 
             throw Error('Could not fetch the data for that resource')
@@ -19,10 +21,16 @@ const useFetch = (url) =>{
           setError(null)
         }
         ).catch(err=>{
-          setError(err.message)
-          setIsPending(false)
-          setData()
+            if (err.name === 'AbortError') {
+                console.log('fetch aborted')
+            }else{
+                setError(err.message)
+                setIsPending(false)
+                setData()
+            }
+          
         })
+        return ()=> abortCont.abort()
       },[url]
       )
 
