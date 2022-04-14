aace8b6e4d24b0308d1ece05ba410e5532c1237a
B-lovedth <greatsoma2019@gmail.com>
2022-04-14 20:04:34 +0100

    sub commit

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import useFetch from './useFetch';
 
 function BlogDetails() {
     const { id } = useParams();
+    const { data, error, isPending } = useFetch( ' http://localhost:8000/blogs/' + id)
     return (
         <div className="blog-details">
             <h2>Blog details - { id }</h2>
