ec305237ecef69c95824783694f87040c81c0076
B-lovedth <greatsoma2019@gmail.com>
2022-04-14 21:16:32 +0100

    sub commit

--- src/Create.js
+++ src/Create.js
@@ -4,6 +4,13 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+          type="text"
+          required
+         />
+      </form>
     </div>
   )
 }
