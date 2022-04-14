25cc187c4b104c4916efaa20d66088ecd5fb8933
B-lovedth <greatsoma2019@gmail.com>
2022-04-14 17:16:16 +0100

    changes

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 
 function BlogDetails() {
+    const { id } = useParams();
     return (
         <div className="blog-details">
-            <h2>Blog details</h2>
+            <h2>Blog details - { id }</h2>
         </div>
     )
 }
