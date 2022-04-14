6b47ed6d0413c2c9e4eacdfcf7d044417a72505a
B-lovedth <greatsoma2019@gmail.com>
2022-04-14 16:52:20 +0100

    changes

---
+++ src/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogDetails() {
+    return (
+        <div className="blog-details">
+            <h2>Blog details</h2>
+        </div>
+    )
+}
+
+export default BlogDetails
