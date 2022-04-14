1a70eb1fef87dd5eb363c8448a27f6aab37c8053
B-lovedth <greatsoma2019@gmail.com>
2022-04-14 21:02:39 +0100

    sub commit

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,7 +4,7 @@ import useFetch from './useFetch';
 
 function BlogDetails() {
     const { id } = useParams();
-    const { data:blog , error, isPending } = useFetch( ' http://localhost:8000/blogs/' + id)
+    const { data:blog , isPending, error } = useFetch( ' http://localhost:8000/blogs/' + id)
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
