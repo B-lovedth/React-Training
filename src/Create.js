d5657307937bc83b221fd61f0ae1078f7f90a269
B-lovedth <greatsoma2019@gmail.com>
2022-04-15 20:16:43 +0100

    sub changes

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,10 @@
-import React from 'react'
 import { useState } from 'react'
 
-const Create = () => {
-  const {title,setTitle} = useState('')
-  const {body,setBody} = useState('')
-  const {author,setAuthor} = useState('')
+function Create() {
+  const { title, setTitle } = useState('')
+  const { body, setBody } = useState('')
+  const { author, setAuthor } = useState('mario')
+  console.log(author)
 
 
   return (
@@ -12,25 +12,29 @@ const Create = () => {
       <h2>Add a New Blog</h2>
       <form>
         <label>Blog title:</label>
-        <input 
+        <input
           type="text"
           required
           value={title}
-          onChange={(e)=> setTitle(e.target.value)}
-         />
+          onChange={(e) => setTitle(e.target.value)} />
         <label>Blog body:</label>
         <textarea
-         required
-         value={body}
-         onChange={()=>setBody(e.target.value)}
-         ></textarea>
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="soma">soma</option>
         </select>
         <button>Add blog</button>
       </form>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author} is the author of this blog</p>
     </div>
   )
 }
