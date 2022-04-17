35c12a64a3241aa273c3c47c65e54a373fba544c
B-lovedth <greatsoma2019@gmail.com>
2022-04-17 22:23:47 +0100

    changes

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const NotFound = () => {
   return (
     <div className='not-found'>
-    <h2>Sorry</h2>  
+    <h2>Sorry😥</h2>  
     <p>That page cannot be found</p>
     <Link to='/'>Back to Homepage...</Link>
     </div>
