6b4eae8f85b74ce6f4841ea355f2f02655979e6f
B-lovedth <greatsoma2019@gmail.com>
2022-04-18 19:42:55 +0100

    modifications

---
+++ src/Light/Home.js
@@ -0,0 +1,17 @@
+import Bloglist from './Bloglist'
+import useFetch from './useFetch'
+
+const Home = () => {
+   const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+   
+  return (
+    <div className="Home">
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <Bloglist blogs={blogs} title="My Blogs"/>}
+    </div>
+  )
+  }
+
+
+export default Home
