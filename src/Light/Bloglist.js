0636dedfc0e13d899c0585aea9a5d7d630b747f8
B-lovedth <greatsoma2019@gmail.com>
2022-04-18 19:42:27 +0100

    modifications

---
+++ src/Light/Bloglist.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Bloglist = ({ blogs,title }) => {
+
+  return (
+    <div className="blog-list">
+        <h2>{ title }</h2>
+         {blogs.map((myblog)=>(
+            <div className="blog-preveiw" key={myblog.id}>
+                <Link to={`/blogs/${myblog.id}`}>
+                  <h2>{myblog.title}</h2>
+                  <p>Written by { myblog.author }</p>
+                </Link>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default Bloglist
