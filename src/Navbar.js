6f6127ea6383ee71ea50cfd71600dead19644e3b
B-lovedth <greatsoma2019@gmail.com>
2022-04-19 22:37:58 +0100

    sub commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <h1>The Soma Articles</h1>
+        <h1>Soma Articles</h1>
         <div className="links">
             <Link to="/">Home</Link>
             <Link to="/create" className=''>Create Article</Link>
